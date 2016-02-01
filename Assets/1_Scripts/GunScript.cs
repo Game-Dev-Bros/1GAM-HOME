4d5796d7dd4f67908fd77581d67960c27b56c3d9
Pedro Lucas <pmslucas@gmail.com>
2016-02-01 19:53:24 +0000

    First gun version

---
+++ Assets/1_Scripts/GunScript.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class GunScript : MonoBehaviour {
+
+    
+    public enum GunType
+    {
+        Pistol,
+        Machinegun,
+        Rocketlauncher
+    }
+
+
+    const float PistolROF = 1;
+    const float PistolRange = 200;
+    const float PistolDamage = 3;
+    const float PistolRadius = 0;
+    const float PistolProjectileSpeed = 2000;
+
+    const float MachinegunROF = 3;
+    const float MachinegunRange = 100;
+    const float MachinegunDamage = 2;
+    const float MachinegunRadius = 0;
+    const float MachinegunProjectileSpeed = 2000;
+
+    const float RocketlauncherROF = 0.25f;
+    const float RocketlauncherRange = 400;
+    const float RocketlauncherDamage = 10;
+    const float RocketlauncherRadius = 10;
+    const float RocketlauncherProjectileSpeed = 1000;
+
+
+    public GunType type;
+    public GameObject projectile;
+
+    private float _rateOfFire; //shots per sec
+    private float _range; // so we dont kill the sun by mistake lel
+    private float _damage; //self explanatory
+    private float _radius; // in case we have rocket launcher or other explosives
+    private float _projectileSpeed;
+
+    private bool _hasShot = false;
+
+	// Use this for initialization
+	void Awake () {
+        switch (type)
+        {
+            case GunType.Pistol:
+                _rateOfFire = PistolROF;
+                _range = PistolRange;
+                _damage = PistolDamage;
+                _radius = PistolRadius;
+                _projectileSpeed = PistolProjectileSpeed;
+                break;
+            case GunType.Machinegun:
+                _rateOfFire = MachinegunROF;
+                _range = MachinegunRange;
+                _damage = MachinegunDamage;
+                _radius = MachinegunRadius;
+                _projectileSpeed = MachinegunProjectileSpeed;
+                break;
+            case GunType.Rocketlauncher:
+                _rateOfFire = RocketlauncherROF;
+                _range = RocketlauncherRange;
+                _damage = RocketlauncherDamage;
+                _radius = RocketlauncherRadius;
+                _projectileSpeed = RocketlauncherProjectileSpeed;
+                break;
+            default:
+                _rateOfFire = PistolROF;
+                _range = PistolRange;
+                _damage = PistolDamage;
+                _radius = PistolRadius;
+                _projectileSpeed = PistolProjectileSpeed;
+                break;
+        }
+        
+    }
+
+    public void Fire()
+    {
+        if(!_hasShot)
+            StartCoroutine(FireRoutine());
+    }
+
+
+    IEnumerator FireRoutine()
+    {
+        if (!_hasShot)
+        {
+            _hasShot = true;
+            var bullet = Instantiate(projectile);
+            bullet.transform.position = transform.position;
+            bullet.transform.rotation = transform.rotation;
+            bullet.GetComponent<ProjectileScript>().SetParent(gameObject);
+            bullet.GetComponent<ProjectileScript>().SetRange(_range);
+            var rb = bullet.GetComponent<Rigidbody>();            
+            rb.AddForce(transform.forward*_projectileSpeed, ForceMode.Force);
+            bullet.GetComponent<ProjectileScript>().Shoot();
+
+            var waitTime = 1 / _rateOfFire;
+            while(waitTime > 0)
+            {
+                waitTime -= Time.deltaTime;
+                yield return null;
+            }
+            _hasShot = false;
+        }
+    }
+        	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(0))
+            Fire();
+
+	}
+}
---
+++ Assets/1_Scripts/ProjectileScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileScript : MonoBehaviour {
+
+    private float range;
+    private GameObject parentGun;
+
+	// Use this for initialization
+	void Awake () {    
+        //StartCoroutine(CheckStatus());
+	}
+	
+    public void SetRange(float v)
+    {
+        range = v;
+    }
+
+    public void SetParent(GameObject p)
+    {
+        parentGun = p;
+    }
+
+    public void Shoot()
+    {
+        StartCoroutine(CheckStatus());
+    }
+
+    IEnumerator CheckStatus()
+    {
+        while(Vector3.Magnitude(transform.position-parentGun.transform.position) < range)
+        {
+            yield return new WaitForSeconds(0.2f);
+        }
+        Destroy(gameObject);
+    }
+
+}
