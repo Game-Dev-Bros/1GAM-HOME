015849242c4c14357c4c44e7178f4f46e820e91f
Pedro Miguel Henriques Pereira <pmh.pereira@gmail.com>
2016-02-01 19:51:27 +0000

    Implement Planet's gravity and Player's movement

---
+++ Assets/1_Scripts/PlanetGravity.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlanetGravity : MonoBehaviour
+{
+    public float gravitationalMagnitude = -9.81f; // same as the damn Earthlings are used too!
+    private float radius;
+    private Vector3 center;
+
+    void Awake()
+    {
+        radius = transform.localScale.x / 2;
+        center = transform.position;
+    }
+
+    public Vector3 GetAtmosphericalNormal(Vector3 position)
+    {
+        return (position - center).normalized;
+    }
+
+    public Vector3 GetAtmosphericalGravity(Vector3 position)
+    {
+        return GetAtmosphericalNormal(position) * gravitationalMagnitude;
+    }
+
+    public Vector3 GetSurfaceGravity(Vector3 surfaceNormal)
+    {
+        return surfaceNormal * gravitationalMagnitude;
+    }
+}
---
+++ Assets/1_Scripts/PlayerMovement.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlanetGravity planetGravity;
+
+    private new Rigidbody rigidbody;
+
+    public float movementSpeed = 5; // meters per second
+
+    private bool isAirborne = true; // notice me flying, senpai! ಥ_ಥ
+    private Vector3 surfaceNormal = Vector3.zero;
+
+    void Awake()
+    {
+        planetGravity = GameObject.FindObjectOfType<PlanetGravity>();
+
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ApplyMovement();
+        //ApplyRotation();
+    }
+
+    void ApplyMovement()
+    {
+        if(isAirborne)
+        {
+            return;
+        }
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 transformDiff = Vector3.zero;
+        transformDiff += vertical * transform.forward;
+        transformDiff += horizontal * transform.right;
+        if(transformDiff.magnitude > 1)
+        {
+            transformDiff.Normalize();
+        }
+
+        transform.position += transformDiff * movementSpeed * Time.deltaTime;
+    }
+    
+    void ApplyRotation()
+    {
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.LookAt(mouseWorldPosition, transform.up);
+    }
+
+    void FixedUpdate()
+    {
+        ApplyGravity();
+    }
+
+    void ApplyGravity()
+    {
+        Vector3 gravitationalForce = Vector3.zero;
+
+        if(isAirborne)
+        {
+            gravitationalForce = planetGravity.GetAtmosphericalGravity(transform.position);
+        }
+        else
+        {
+            gravitationalForce = planetGravity.GetSurfaceGravity(surfaceNormal);
+        }
+
+        rigidbody.AddForce(gravitationalForce);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if(collision.transform.tag == planetGravity.tag)
+        {
+            isAirborne = false;
+            surfaceNormal = collision.contacts[0].normal;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if(collision.transform.tag == planetGravity.tag)
+        {
+            isAirborne = true;
+        }
+    }
+}
