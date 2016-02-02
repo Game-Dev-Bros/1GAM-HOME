88fc3cfe17c7c66fb993eba1a7f51e78840e7a3f
Pedro Lucas <pmslucas@gmail.com>
2016-02-02 23:36:45 +0000

    Ship spawning test scene

---
+++ Assets/1_Scripts/EnemyShipScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EnemyShipScript : MonoBehaviour {
+
+    public float speed = 10;
+
+    private GameObject planet;
+    private bool landed = false;
+    
+	// Use this for initialization
+	void Awake () {
+        planet = GameObject.Find("Planet");
+        //StartCoroutine(Kamikaze());
+	}
+
+    private IEnumerator Kamikaze(int steps = 60)
+    {
+        var radiusVector = (transform.position - planet.transform.position).normalized*planet.transform.localScale.x;
+        var finaVector = ((planet.transform.position - transform.position) + radiusVector);
+
+        var step = (finaVector.magnitude * speed) / steps;
+        var pos = 0f;
+        for (int i = 0; i < steps; i++)
+        {
+
+            transform.position = Vector3.Lerp(transform.position, planet.transform.position, pos);
+            pos += step;
+            Debug.Log("moverino");
+            //transform.position = Vector3.MoveTowards(transform.position, planet.transform.position, step);
+            yield return null;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(!landed)
+            transform.position = Vector3.MoveTowards(transform.position, planet.transform.position, speed*Time.deltaTime);
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            landed = true;
+            transform.gameObject.isStatic = true;
+        }
+
+    }
+
+}
---
+++ Assets/1_Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnerScript : MonoBehaviour {
+
+    
+    public GameObject enemyPrefab;
+    public int waveNumber = 0;
+    public float[] waveFrequency = new float[5] {0.2f,0.4f, 0.6f, 1f,1.2f};
+    public float[] waveDuration = new float[5] { 25f, 20f, 16f, 13f, 10f };
+
+    private bool _hasSpawned = false;
+    private GameObject planet;
+
+	// Use this for initialization
+	void Awake () {
+        planet = GameObject.Find("Planet");
+        StartCoroutine(SpawnRoutine());
+	}
+	
+
+    IEnumerator SpawnRoutine()
+    {
+        var duration = waveDuration[waveNumber];
+        while (duration > 0)
+        {
+            duration -= Time.deltaTime;
+            if(!_hasSpawned)
+                StartCoroutine(SpawnOne());
+            yield return null;
+        }
+    }
+
+    IEnumerator SpawnOne()
+    {
+        if (!_hasSpawned)
+        {
+            _hasSpawned = true;
+            SpawnShip();
+            var waitTime = 1 / waveFrequency[waveNumber];
+            while (waitTime > 0)
+            {
+                waitTime -= Time.deltaTime;
+                yield return null;
+            }
+            _hasSpawned = false;
+        }
+    }
+    
+    void SpawnShip()
+    {
+        var randomLoc = Random.onUnitSphere * planet.transform.localScale.x * 2f;
+        GameObject enemy = Instantiate(enemyPrefab);
+        enemy.transform.parent = gameObject.transform;
+        enemy.transform.localPosition = randomLoc;
+    }
+
+
+}
