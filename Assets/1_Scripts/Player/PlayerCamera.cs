7953389de20ef5892a3af5de4e7b868b7fd15e71
Pedro Miguel Henriques Pereira <pmh.pereira@gmail.com>
2016-02-03 17:07:45 +0000

    Moved mouse tracking in camera to PlayerCamera

--- Assets/1_Scripts/Player/PlayerCamera.cs
+++ Assets/1_Scripts/Player/PlayerCamera.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
     private GameObject player;
-    public float height;
+
+    public float cameraHeight = 10;
+    public float maxDistanceAdjustment = 15;
+    public float maxDegreeAdjustment = 5;
+
+    public bool adjustToMouse = true;
+
+    public bool useSlerp = true;
+    public float slerpMultiplier = 3;
 
 	void Awake ()
     {
@@ -13,10 +21,37 @@ public class PlayerCamera : MonoBehaviour
 	
 	void FixedUpdate ()
     {
-        Vector3 targetPosition = player.transform.position + player.transform.up * height;
-        transform.position = targetPosition;
+        Vector3 targetPosition = player.transform.position + player.transform.up * cameraHeight;
 
-        Quaternion targetRotation = player.transform.rotation * Quaternion.Euler(90, 0, 0);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 3f);    
+        Vector3 origin = player.transform.position + player.transform.up * player.transform.localScale.y / 2;
+        Plane plane = new Plane(player.transform.up, origin);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Quaternion targetRotation = player.transform.rotation;
+
+        if(adjustToMouse)
+        {
+            float distance;
+            if(plane.Raycast(ray, out distance))
+            {
+                Vector3 difference = (ray.GetPoint(distance) - transform.position);
+                Vector3 direction = difference.normalized;
+
+                Vector2 eulerAdjustement = difference;
+                eulerAdjustement.x = Mathf.Max(-maxDistanceAdjustment, Mathf.Min(eulerAdjustement.x, maxDistanceAdjustment)) / maxDistanceAdjustment;
+                eulerAdjustement.y = -Mathf.Max(-maxDistanceAdjustment, Mathf.Min(eulerAdjustement.y, maxDistanceAdjustment)) / maxDistanceAdjustment;
+            
+                eulerAdjustement *=  maxDegreeAdjustment;
+
+                targetRotation *= Quaternion.Euler(eulerAdjustement.y, 0, eulerAdjustement.x); 
+            }
+        }
+
+        targetRotation *= Quaternion.Euler(90, 0, 0);
+
+        float slerpT = (useSlerp ? (Time.deltaTime * slerpMultiplier) : 1);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, slerpT);
+
+        transform.position = targetPosition;
 	}
 }
--- Assets/1_Scripts/Player/PlayerRotation.cs
+++ Assets/1_Scripts/Player/PlayerRotation.cs
@@ -1,15 +1,7 @@
 ﻿using UnityEngine;
 
-public class PlayerRotation : MonoBehaviour {
-
-    public float maxDistanceAjustment = 15;
-    public float cameraOffsetAmount = 0.5f;
-
-    void Start()
-    {
-        cameraOffsetAmount = Mathf.Clamp(cameraOffsetAmount, 0f, 1f);
-    }
-
+public class PlayerRotation : MonoBehaviour
+{
     void Update()
     {
         ApplyRotation();
@@ -24,19 +16,8 @@ public class PlayerRotation : MonoBehaviour {
         float distance;
         if(plane.Raycast(ray, out distance))
         {
-            Debug.Log("Distance: " + distance);
-            Debug.Log("Ray: " + ray.GetPoint(distance).ToString());
-
             Vector3 direction = (ray.GetPoint(distance) - origin).normalized;
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direction, transform.up), Time.deltaTime * 5);
-
-            Vector3 halfWay;
-            if (distance < maxDistanceAjustment)
-                halfWay = (ray.GetPoint(distance) - transform.position) * cameraOffsetAmount;
-            else
-                halfWay = (ray.GetPoint(maxDistanceAjustment) - transform.position) * cameraOffsetAmount;
-            Camera.main.transform.LookAt(halfWay);
         }
     }
-
 }
